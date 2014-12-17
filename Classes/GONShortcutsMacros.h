593606c09bcf3f0162534407424a89f9385be4b7
nicolasgoutaland <nicolas.goutaland@gmail.com>
2014-12-17 14:19:09 +0100

    Updated macros

--- Classes/GONShortcutsMacros.h
+++ Classes/GONShortcutsMacros.h
@@ -45,10 +45,12 @@
 #define DEVICE_SYSTEM_VERSION                       [CURRENT_DEVICE systemVersion]
 
 // Device screen specifications / interface idiom
-#define IS_RETINA_SCREEN()                          (MAIN_SCREEN.scale == 2.0)
+#define IS_RETINA_SCREEN()                          (MAIN_SCREEN.scale > 1.0)
 #define IS_IPHONE() 								(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
 #define IS_IPAD()		 							(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
 #define IS_IPHONE_5()  								((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) && [UIScreen mainScreen].bounds.size.height == 568)
+#define IS_IPHONE_6()  								((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) && [UIScreen mainScreen].bounds.size.height == 667)
+#define IS_IPHONE_6_PLUS()                          ((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) && [UIScreen mainScreen].bounds.size.height == 736)
 
 // System version
 #define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[CURRENT_DEVICE systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
@@ -56,3 +58,5 @@
 #define SYSTEM_VERSION_IS_IOS5_OR_GREATER()          SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"5.0")
 #define SYSTEM_VERSION_IS_IOS6_OR_GREATER()          SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"6.0")
 #define SYSTEM_VERSION_IS_IOS7_OR_GREATER()          SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")
+#define SYSTEM_VERSION_IS_IOS8_OR_GREATER()          SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0")
+#define SYSTEM_VERSION_IS_IOS9_OR_GREATER()          SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"9.0")
