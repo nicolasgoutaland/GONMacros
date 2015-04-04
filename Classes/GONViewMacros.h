3473f5d69977ccac6408bf637b5e29f94f534731
nicolasgoutaland <nicolas.goutaland@gmail.com>
2015-04-04 19:49:14 +0200

    Added view bounds manipulation macros

--- Classes/GONViewMacros.h
+++ Classes/GONViewMacros.h
@@ -21,6 +21,22 @@
 #define UPDATE_VIEW_FRAME_SIZE(aView,newSize)       do{CGRect __tmpFrame = aView.frame; __tmpFrame.size = (newSize); aView.frame = __tmpFrame;}while(0)
 #define UPDATE_VIEW_FRAME_ORIGIN(aView,newOrigin)   do{CGRect __tmpFrame = aView.frame; __tmpFrame.origin = (newOrigin); aView.frame = __tmpFrame;}while(0)
 
+// Update current bounds
+#define UPDATE_BOUNDS_X(newX)						do{CGRect __tmpBounds = self.bounds; __tmpBounds.origin.x = (newX); self.bounds = __tmpBounds;}while(0)
+#define UPDATE_BOUNDS_Y(newY)						do{CGRect __tmpBounds = self.bounds; __tmpBounds.origin.y = (newY); self.bounds = __tmpBounds;}while(0)
+#define UPDATE_BOUNDS_WIDTH(newWidth)				do{CGRect __tmpBounds = self.bounds; __tmpBounds.size.width = (newWidth); self.bounds = __tmpBounds;}while(0)
+#define UPDATE_BOUNDS_HEIGHT(newHeight)				do{CGRect __tmpBounds = self.bounds; __tmpBounds.size.height = (newHeight); self.bounds = __tmpBounds;}while(0)
+#define UPDATE_BOUNDS_SIZE(newSize)                 do{CGRect __tmpBounds = self.bounds; __tmpBounds.size = (newSize); self.bounds = __tmpBounds;}while(0)
+#define UPDATE_BOUNDS_ORIGIN(newOrigin)				do{CGRect __tmpBounds = self.bounds; __tmpBounds.origin = (newOrigin); self.bounds = __tmpBounds;}while(0)
+
+// Update a view bounds
+#define UPDATE_VIEW_BOUNDS_X(aView, newX)			do{CGRect __tmpBounds = aView.bounds; __tmpBounds.origin.x = (newX); aView.bounds = __tmpBounds;}while(0)
+#define UPDATE_VIEW_BOUNDS_Y(aView, newY)			do{CGRect __tmpBounds = aView.bounds; __tmpBounds.origin.y = (newY); aView.bounds = __tmpBounds;}while(0)
+#define UPDATE_VIEW_BOUNDS_WIDTH(aView, newWidth)	do{CGRect __tmpBounds = aView.bounds; __tmpBounds.size.width = (newWidth); aView.bounds = __tmpBounds;}while(0)
+#define UPDATE_VIEW_BOUNDS_HEIGHT(aView, newHeight)	do{CGRect __tmpBounds = aView.bounds; __tmpBounds.size.height = (newHeight); aView.bounds = __tmpBounds;}while(0)
+#define UPDATE_VIEW_BOUNDS_SIZE(aView,newSize)      do{CGRect __tmpBounds = aView.bounds; __tmpBounds.size = (newSize); aView.bounds = __tmpBounds;}while(0)
+#define UPDATE_VIEW_BOUNDS_ORIGIN(aView,newOrigin)  do{CGRect __tmpBounds = aView.bounds; __tmpBounds.origin = (newOrigin); aView.bounds = __tmpBounds;}while(0)
+
 // Update current center
 #define UPDATE_CENTER_X(newX)						do{CGPoint __tmpCenter = self.center; __tmpCenter.x = (newX); self.center = __tmpCenter;}while(0)
 #define UPDATE_CENTER_Y(newY)						do{CGPoint __tmpCenter = self.center; __tmpCenter.y = (newY); self.center = __tmpCenter;}while(0)
